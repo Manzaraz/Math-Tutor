875188b941a3c12e8882796088a4db6191ca9a95
Christian Manzaraz <magnusmanz@gmail.com>
2023-12-31 18:07:58 -0300

    Initial Commit

---
+++ Math Tutor/Math_TutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Math_TutorApp.swift
+//  Math Tutor
+//
+//  Created by Christian Manzaraz on 31/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Math_TutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
